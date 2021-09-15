8d8d4664b50711af4b8e36a7048870c26bfcce79
Robert Grozea <robert.grozea@gmail.com>
2021-09-15 15:50:59 +0300

    Added the account withdrawal test/func

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -3,6 +3,10 @@ package org.xpdojo.bank;
 public class Account {
     public int balance;
 
+    public static void main(String[] args) {
+
+    }
+
     public void deposit(int i) {
         this.balance += i;
     }
